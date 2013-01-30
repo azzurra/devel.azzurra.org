aab3aa23e03c41fbd093c11b51f2789fed229eb1
Matteo Panella <morpheus@azzurra.org>
2013-01-30 12:37:49 +0100

    Add Google Custom Search

---
+++ content/js/cse.js
@@ -0,0 +1,10 @@
+/*
+ * Code for Google Custom Search Engine
+ */
+(function() {
+  var cx = '012357419976593811995:q1itwatzbua';
+  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+      '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
